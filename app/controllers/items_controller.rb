92be73d61278f241582d79c8e1e1183a7162f949
[tishpr] <[tishsny@gmail.com]>
2015-05-23 22:28:06 -0700

    add destroy js, not working yet

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @items = @user.items
-
     @item = current_user.items.build( item_params )
     @item.user = @user
     @new_item = Item.new
@@ -18,6 +17,25 @@ class ItemsController < ApplicationController
         format.js
       end
   end
+
+     def destroy
+      @user = User.find(params[:user_id])
+     @item = @user.items.find(params[:id])
+    
+     if @item.destroy
+       flash[:notice] = "Item was removed."
+       
+     else
+       flash[:error] = "Item couldn't be deleted. Try again."
+       
+     end
+
+     respond_to do |format|
+       format.html
+       format.js
+     end
+   end 
+
 
   private 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:update, :show] do
-    resources :items, only: [:create] 
+    resources :items, only: [:create, :destroy] 
   end
   root to: 'welcome#index'
 end
