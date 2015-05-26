b85cfc58d93652677bd9e49f3e9997209567ba68
[tishpr] <[tishsny@gmail.com]>
2015-05-26 00:25:15 -0700

    properly seeded fake data with random times created

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ require 'faker'
   # Create Users
  5.times do
    user = User.new(
-
      name:     Faker::Name.name,
      email:    Faker::Internet.email,
      password: Faker::Lorem.characters(10)
@@ -12,8 +11,8 @@ require 'faker'
    user.skip_confirmation!
    user.save!
  end
- users = User.all
- 
+
+ users = User.all 
     # Create an admin user
      admin = User.new(
        name:     'Admin User',
@@ -25,11 +24,12 @@ require 'faker'
     
  # Create Items
  10.times do
-   Item.create!(
-     name:   Faker::Lorem.sentence,
+   item = Item.create!(
+     name: Faker::Lorem.sentence,
      user: admin
      )
-   
+   item.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+   item.save!  
  end
  items = Item.all
 
